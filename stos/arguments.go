128e933d4b8cccc9393b7d6cd93bd5e0065822b9
vchatchai <ee56054@gmail.com>
2018-09-28 23:26:29 +0700

    update arguments

--- stos/arguments.go
+++ stos/arguments.go
@@ -7,12 +7,15 @@ import (
 
 func Arguments() {
 	args := os.Args
+	if len(args) == 0 {
+		return
+	}
 
 	fmt.Println(args)
 
 	//The first argument, zero item from slice
 	programNme := args[0]
-	fmt.Println("The binary name is: %s \n", programNme)
+	fmt.Printf("The binary name is: %s \n", programNme)
 
 	//The rest of the  argments could be obtained
 	//by omitting the first arguments.
