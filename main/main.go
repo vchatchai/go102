b35b1db5d641b22a34d6f133fd976d1ad4608dc6
vchatchai <ee56054@gmail.com>
2018-10-01 14:31:10 +0700

    add external process asynchronous

--- main/main.go
+++ main/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/vchatchai/go102/stos/stsignal"
+import "github.com/vchatchai/go102/stos/stexec"
 
 func main() {
 	// stos.Arguments()
 	// stflag.Flag()
-	stsignal.Signal()
+	// stsignal.Signal()
+
+	// stexec.Exec()
+
+	stexec.Asynchronous()
 }
---
+++ stos/stexec/asynchronous.go
@@ -0,0 +1,24 @@
+package stexec
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
+func Asynchronous() {
+	prc := exec.Command("ls", "-a")
+	out := bytes.NewBuffer([]byte{})
+	prc.Stdout = out
+	err := prc.Start()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	prc.Wait()
+
+	if prc.ProcessState.Success() {
+		fmt.Println("Process run successfully with output:\n")
+		fmt.Println(prc.Stdout)
+	}
+}
