e9d23190a7bfadb4aa9a66bdfae55485dec0e569
vchatchai <ee56054@gmail.com>
2018-10-23 11:40:35 +0700

    update parse

--- sttime/parse.go
+++ sttime/parse.go
@@ -12,6 +12,12 @@ func Parse() {
 	}
 	fmt.Println(t)
 
+	t, err = time.Parse("2/1/2006 15:04:05 MST", "22/10/2018 22:25:11 UTC")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(t)
+
 	t, err = time.ParseInLocation("2/1/2006 3:04 PM", "31/7/2018 11:48 AM", time.Local)
 	if err != nil {
 		panic(err)
